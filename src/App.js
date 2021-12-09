b924283f050d915d3a245bba61bb1a360a8927ef
conuko <constantinu93@gmail.com>
2021-12-09 08:51:55 +0100

    feat: complete WeatherCard and pass hardcoded values

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import WeatherCard from './components/WeatherCard/WeatherCard';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">This is my weather App</div>
+    <div className="App">
+      <WeatherCard
+        dt={1602104400 * 1000}
+        tempMin="22.67"
+        tempMax="24.39"
+        main="Clear"
+        icon="01d"
+      />
+    </div>
   );
 };
 
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
-const WeatherCard = (props) => (
-  <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px180" />
-    <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-    </Card.Body>
-  </Card>
-);
+const WeatherCard = ({
+  dt, tempMin, tempMax, main, icon,
+}) => {
+  // create date object
+  const date = new Date(dt);
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img
+        variant="top"
+        // get source from openweathermap url and pass icon prop to get correct icon
+        src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+      />
+      <Card.Body>
+        <Card.Title>{main}</Card.Title>
+        {/* convert date and time to local format */}
+        <p>
+          {date.toLocaleDateString()}
+          {' '}
+          -
+          {' '}
+          {date.toLocaleTimeString()}
+        </p>
+        {/* minimum temperature */}
+        <p>
+          Min:
+          {' '}
+          {tempMin}
+        </p>
+        {/* maximum temperature */}
+        <p>
+          Max:
+          {' '}
+          {tempMax}
+        </p>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default WeatherCard;
