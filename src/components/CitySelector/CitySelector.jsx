d279e44495ed8a76b3ef3ce5ee4af5c113516441
conuko <constantinu93@gmail.com>
2021-12-09 08:57:53 +0100

    feat: create CitySelector

---
+++ src/components/CitySelector/CitySelector.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+const CitySelector = () => {
+  const [city, setCity] = useState('');
+  return <div></div>
+};
+
+export default CitySelector;
--- src/components/WeatherCard/WeatherCard.styles.jsx
+++
@@ -1,1 +0,0 @@
-// Add styled component here!
