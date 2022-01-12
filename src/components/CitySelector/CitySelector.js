ee58d4b6c1491de8bb522edb2f7642bfeb8dc47b
Constantin <constantinu93@gmail.com>
2022-01-12 17:14:57 +0100

    style: configure responsive design for CitySelector

--- src/components/CitySelector/CitySelector.js
+++ src/components/CitySelector/CitySelector.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
-import { H2 } from '../_base/text/Text';
-import { CheckWeatherButton, CheckWeatherInput, Container } from './CitySelector.styles';
+import React, { useState } from 'react'
+import { H2 } from '../_base/text/Text'
+import {
+  CheckWeatherButton,
+  CheckWeatherInput,
+  Container,
+} from './CitySelector.styles'
 
 const CitySelector = ({ onSearch }) => {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState('')
 
   const onKeyDown = (e) => {
     if (e.keyCode === 13) {
-      onSearch(city);
+      onSearch(city)
     }
-  };
+  }
 
   return (
     <>
@@ -32,16 +36,15 @@ const CitySelector = ({ onSearch }) => {
             />
           </form>
         </div>
-      </Container>
-
-      <Container>
         <div>
           {/* The event handler to handle the button click */}
-          <CheckWeatherButton type="button" onClick={() => onSearch(city)}>Check Weather</CheckWeatherButton>
+          <CheckWeatherButton type="button" onClick={() => onSearch(city)}>
+            Check Weather
+          </CheckWeatherButton>
         </div>
       </Container>
     </>
-  );
-};
+  )
+}
 
-export default CitySelector;
+export default CitySelector
--- src/components/CitySelector/CitySelector.styles.js
+++ src/components/CitySelector/CitySelector.styles.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import { theme } from '../../styles';
+import styled from 'styled-components'
+import { theme } from '../../styles'
 
 export const CheckWeatherButton = styled.button`
   border: none;
@@ -15,13 +15,13 @@ export const CheckWeatherButton = styled.button`
   &:hover {
     opacity: 1;
   }
-`;
+`
 
 export const CheckWeatherInput = styled.input`
   border-radius: ${theme.border.radius};
   font-size: ${theme.font.size.h5};
   font-weight: ${theme.font.weight.bold};
-  transition: all .30s ease-in-out;
+  transition: all 0.3s ease-in-out;
   border: 3px solid #000;
   color: ${theme.color.red};
   background-color: ${theme.color.white};
@@ -29,11 +29,15 @@ export const CheckWeatherInput = styled.input`
   margin-bottom: 1rem;
   outline: none;
   padding: 1rem 0 1rem 1rem;
-  transition: all .3s ease;
+  transition: all 0.3s ease;
   width: 100%;
-`;
+`
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-`;
+  @media (min-width: 768px) {
+    flex-wrap: wrap;
+    align-content: center;
+  }
+`
